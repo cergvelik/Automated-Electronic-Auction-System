0a3ec8eb681c77bf329f743609a3454d3d3556a6
admiralkenobii <98663564+admiralkenobii@users.noreply.github.com>
2024-04-29 09:18:00 +0700

    Update AuctionRepositoryInSQL.cs

--- ElectronicAuction/Classes/RepositoryClasses/AuctionRepositoryInSQL.cs
+++ ElectronicAuction/Classes/RepositoryClasses/AuctionRepositoryInSQL.cs
@@ -1,6 +1,7 @@
-﻿using ElectronicAuction.Interfaces.AuctionInterfaces;
+using ElectronicAuction.Interfaces.AuctionInterfaces;
 using ElectronicAuction.Interfaces.RepositoryInterfaces;
 using ElectronicAuction.Classes.AuctionClasses;
+using System.Data.SqlClient;
 
 namespace ElectronicAuction.Classes.RepositoryClasses
 {
@@ -8,9 +9,26 @@ namespace ElectronicAuction.Classes.RepositoryClasses
     {
         private readonly string _connectionString;
 
-        public AuctionRepositoryInSQL() { } //конструктор класса
+        public AuctionRepositoryInSQL(string connectionString) 
+        {
+            _connectionString = connectionString;
+        } //конструктор класса
 
-        public void AddAuctionWithBid(IAuctionWithBid auction) { } //добавление аукциона со ставкой в базу данных
+        public void AddAuctionWithBid(IAuctionWithBid auction)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Auctions (AuctionId, StartDate, EndDate, AuctionCreator) VALUES (@AuctionId, @StartDate, @EndDate, @AuctionCreator)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@AuctionId", auction.AuctionId);
+                command.Parameters.AddWithValue("@StartDate", auction.StartDate);
+                command.Parameters.AddWithValue("@EndDate", auction.EndDate);
+                command.Parameters.AddWithValue("@AuctionCreator", auction.AuctionCreator);
+                // ещо
+                command.ExecuteNonQuery();
+            }
+        } //добавление аукциона со ставкой в базу данных
 
         public void ReturnAuctionWithBid(int id, IAuctionWithBid auction) { } //обновление аукциона со ставкой в базе
 
