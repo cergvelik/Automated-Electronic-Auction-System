9788e4aca3d1f8ee10031b6b8f312736fac3a531
admiralkenobii <98663564+admiralkenobii@users.noreply.github.com>
2024-04-29 09:19:13 +0700

    Update Program.cs

--- ElectronicAuction/Program.cs
+++ ElectronicAuction/Program.cs
@@ -1,12 +1,61 @@
-﻿
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using System.Data.SqlClient;
 
 namespace ElectronicAuction
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            
+        // LAPTOP - B4VML6MR\SQLEXPRESS
+
+            static void Main(string[] args)
+            {
+                // введите свой
+                string connectionString = "Data Source= LAPTOP-B4VML6MR\\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";
+                RunSqlQueries(connectionString);
+            }
+
+            static void RunSqlQueries(string connectionString)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Введите SQL запрос (или 'exit' для выхода):");
+                    string query = Console.ReadLine();
+
+                    if (query.ToLower() == "exit")
+                        break;
+
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            SqlCommand command = new SqlCommand(query, connection);
+                            connection.Open();
+
+                            SqlDataReader reader = command.ExecuteReader();
+
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write(reader[i] + "\t");
+                                }
+                                Console.WriteLine();
+                            }
+
+                            reader.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Ошибка: " + ex.Message);
+                    }
+                }
+            }
         }
+
     }
-}
+
