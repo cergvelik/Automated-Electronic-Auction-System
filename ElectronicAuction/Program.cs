439dd87759eb69fc4e7edfec61ba180afcafd41a
admiralkenobii <98663564+admiralkenobii@users.noreply.github.com>
2024-05-07 01:56:56 +0700

    Update Program.cs

--- ElectronicAuction/Program.cs
+++ ElectronicAuction/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;*/
+using ElectronicAuction.Classes.Account;
+using ElectronicAuction.Classes.Services;
+using ElectronicAuction.Interfaces.ServicesInterfaces;
 using System.Data.SqlClient;
 
 namespace ElectronicAuction
@@ -15,45 +18,50 @@ namespace ElectronicAuction
             {
                 // введите свой
                 string connectionString = "Data Source= LAPTOP-B4VML6MR\\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";
-                RunSqlQueries(connectionString);
+                ConsolePersonalAccount cpa = new ConsolePersonalAccount(); 
+                RunSqlQueries(connectionString, cpa);
             }
 
-            static void RunSqlQueries(string connectionString)
+            static void RunSqlQueries(string connectionString, ConsolePersonalAccount cpa)
             {
+            
+                //cpa.InitializateAccount();
                 while (true)
                 {
-                    Console.WriteLine("Введите SQL запрос (или 'exit' для выхода):");
-                    string query = Console.ReadLine();
-
-                    if (query.ToLower() == "exit")
-                        break;
-
                     try
                     {
                         using (SqlConnection connection = new SqlConnection(connectionString))
                         {
-                            SqlCommand command = new SqlCommand(query, connection);
-                            connection.Open();
+                            cpa.AccountMenu(connectionString);
+                            /*Console.WriteLine("Введите SQL запрос (или 'exit' для выхода):");
+                            string query = Console.ReadLine();
 
-                            SqlDataReader reader = command.ExecuteReader();
+                            if (query.ToLower() == "exit")
+                                break;
 
-                            while (reader.Read())
-                            {
-                                for (int i = 0; i < reader.FieldCount; i++)
-                                {
-                                    Console.Write(reader[i] + "\t");
-                                }
-                                Console.WriteLine();
-                            }
+                                    SqlCommand command = new SqlCommand(query, connection);
+                                    connection.Open();
 
-                            reader.Close();
+                                    SqlDataReader reader = command.ExecuteReader();
+
+                                    while (reader.Read())
+                                    {
+                                        for (int i = 0; i < reader.FieldCount; i++)
+                                        {
+                                            Console.Write(reader[i] + "\t");
+                                        }
+                                        Console.WriteLine();
+                                    }
+
+                                    reader.Close();
+                            */
                         }
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine("Ошибка: " + ex.Message);
                     }
-                }
+                } 
             }
         }
 
