b027ca483376e2eb95b77cf096694de11f71a1e7
admiralkenobii <98663564+admiralkenobii@users.noreply.github.com>
2024-05-07 12:27:52 +0700

    Update AuctionInfoService.cs

--- ElectronicAuction/Classes/SecondaryServices/AuctionInfoService.cs
+++ ElectronicAuction/Classes/SecondaryServices/AuctionInfoService.cs
@@ -8,6 +8,8 @@ namespace ElectronicAuction.Classes.SecondaryServices
     {
         private readonly IAuctionRepository _auctionRepository;
 
+        public AuctionInfoService(IAuctionRepository auctionRepository) { _auctionRepository = auctionRepository; }
+
         public AuctionInfo GetInfoAboutAuction(int id) { return null; } //получение информации об аукционе
 
         public List<AuctionInfo> GetInfoAboutAllAuctions() { return null; } //получение информации о всех аукционах
