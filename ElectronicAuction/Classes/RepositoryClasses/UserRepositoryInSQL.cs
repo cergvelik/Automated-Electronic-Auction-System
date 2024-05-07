e69668b687f83bf58643c4b8223ae85eef65c4d9
admiralkenobii <98663564+admiralkenobii@users.noreply.github.com>
2024-05-07 12:35:17 +0700

    Update UserRepositoryInSQL.cs

--- ElectronicAuction/Classes/RepositoryClasses/UserRepositoryInSQL.cs
+++ ElectronicAuction/Classes/RepositoryClasses/UserRepositoryInSQL.cs
@@ -13,6 +13,8 @@ namespace ElectronicAuction.Classes.RepositoryClasses
 
         public IUser GetUser(int id) { return null; } //получение пользователя по его id
 
+        public IUser GetUser(string email, string password) { return null; } //получение пользователя по его email и паролю
+
         public void AddUser(IUser user) { } //добавление пользователя в базу
 
         public List<IUser> GetAllUsers() { return null; }
